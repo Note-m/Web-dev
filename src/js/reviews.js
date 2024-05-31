219d3df5badf410390e54bccaacfe168c6f2691a
AliceMozyrko <mozyrkoalisa@gmail.com>
2024-05-31 16:46:03 +0300

    update js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,14 +1,99 @@
-// import Swiper JS
 import Swiper from 'swiper';
-// import Swiper styles
 import 'swiper/css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const swiper = new Swiper('.swiper', {
+
+const BASE_URL = "https://portfolio-js.b.goit.study/api"
+
+const nextBtn = document.querySelector(".swiper-button-prev")
+const prevBtn = document.querySelector(".swiper-button-next")
+const galleryReviews = document.querySelector(".reviews-list")
+
+
+const fetchReviews = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/reviews`)
+    if (!response.ok) {
+      throw new Error("Network error")
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Failed fetching", error)
+    iziToast.error({
+      message: "Sorry, failed fetching reviews. Please try again later.",
+      timeout: 2000,
+    });
+    return [];
+  }
+};
+
+const markup = reviews => {
+  return reviews.map(
+    ({ author, avatar_url, review }) => `
+        <li class="user-review">
+          <p class="text-review">${review}</p>
+          <div class="icon-photo-name">
+            <img
+              src="${avatar_url}"
+              alt="${author}"
+              width="40"
+              height="40"
+              class="avatar-icon"
+            />
+            <p class="user-name-review">${author}</p>
+          </div>
+      </li>`
+  ).join("")
+};
+
+const createReviews = async () => {
+  const reviews = await fetchReviews()
+  if (reviews.length === 0) {
+    galleryReviews.innerHTML = "<p class='not-found'>Not found</p>"
+  } else {
+    galleryReviews.innerHTML = markup(reviews)
+    initSwiper()
+  }
+}
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  speed: 400,
-  spaceBetween: 100,
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 'auto',
+    loop: false,
+    allowTouchMove: true,
+    keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    },
+    on: {
+      reachEnd: () => {
+        nextBtn.classList.add('disabled');
+      },
+      reachBeginning: () => {
+        prevBtn.classList.add('disabled');
+      },
+      fromEdge: () => {
+        nextBtn.classList.remove('disabled');
+        prevBtn.classList.remove('disabled');
+      }
+    },
 });
+}
+createReviews()
 
+// nextBtn.addEventListener("click", () => {
+  
+// })
+
+// prevBtnBtn.addEventListener("click", () => {
+  
+// })
+// return swagger.get("api/", {})
