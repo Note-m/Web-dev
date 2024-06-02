46191dd6136d883d15a2c789b3a11916055fa708
Note-m <ant.myro.shop@gmail.com>
2024-06-02 00:11:59 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// import { buttonFaq } from './js/faq.js';
-// import { manageModal } from './js/work-together.js';
-// buttonFaq();
-// manageModal();
-// // import { loadMoreProjects } from './js/my-projects.js';
-// // loadMoreProjects();
+import { buttonFaq } from './js/faq.js';
+import { manageModal } from './js/work-together.js';
+buttonFaq();
+manageModal();
+import { loadMoreProjects } from './js/my-projects.js';
+loadMoreProjects();
